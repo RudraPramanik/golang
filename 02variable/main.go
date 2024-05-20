97a6f31c6a30f3d40198ca7af706c57ca71cb60a
Rudra <rudrahimangshoo2020@gmail.com>
2024-05-20 17:42:18 +0600

    go-type

---
+++ 02variable/main.go
@@ -0,0 +1,43 @@
+package main
+
+import "fmt"
+
+// public variable declare with capital latter
+const LoginToken string = "sfmksmkf"
+
+func main() {
+	var username string = "Rudra"
+	fmt.Println(username)
+	fmt.Printf("variable is of type: %T \n", username)
+
+	var isLoggedIn bool = true
+	fmt.Println(isLoggedIn)
+	fmt.Printf("variable is of type: %T \n", isLoggedIn)
+
+	var samallValue uint8 = 255
+	fmt.Println(samallValue)
+	fmt.Printf("variable is of type: %T \n", samallValue)
+
+	var samallFloat float32 = 255.985735737
+	fmt.Println(samallFloat)
+	fmt.Printf("variable is of type: %T \n", samallFloat)
+
+	//default value and alias
+	var anotherVariable int
+	fmt.Println(anotherVariable)
+	fmt.Printf("another variable : %T \n", anotherVariable)
+
+	var anotherVariables string
+	fmt.Println(anotherVariables)
+	fmt.Printf("another variable : %T \n", anotherVariables)
+
+	//implicit type
+	var web = "rp"
+	fmt.Println(web)
+	fmt.Printf("another variable : %T \n", web)
+
+	//no var
+	numberOfUser := 3000
+	fmt.Println(numberOfUser)
+
+}
