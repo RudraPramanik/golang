6f94ece26bfe67665d29c67f1d7d75f9b4522746
Rudra Pramanik <rudrahimangshoo2020@gmail.com>
2025-04-14 15:56:27 +0600

    ch1

---
+++ ch1/declaration.go
@@ -0,0 +1,8 @@
+package main
+
+import "fmt"
+
+func main() {
+	averageOpenRate, displayMessage, okay := .23, ".23is the average open rate of your messages", 5
+	fmt.Println(averageOpenRate, displayMessage ,okay )
+}
