c0bc3ec731ee62061a584d8934916b4182a660ce
Rudra Pramanik <rudrahimangshoo2020@gmail.com>
2025-05-18 16:50:19 +0600

    map

--- map/example.go
+++ map/example.go
@@ -1,9 +1,19 @@
 package main
 
-import "errors"
+import {"errors", "fmt"}
 
 func getUserMap(names []string, phoneNumbers []int) (map[string]user, error) {
-	// ?
+	if len(names) != len(phophoneNumbers){
+		return nil, errors.New("invalid sizes")
+	}
+	userMap := make(map[string]user)
+	for(i:=0; i< len(names); i++){
+		userMap[names[i]] = user{
+			name : names[i],
+			phoneNumber: phoneNumber[i]
+		}
+	}
+	return userMap, nill
 }
 
 type user struct {
